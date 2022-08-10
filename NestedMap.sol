1c2964ad54849cb0e211e8d0d95635497543c9bd
Gayathri Suresh <77912752+gayathrisuresh150501@users.noreply.github.com>
2022-08-10 10:29:26 +0530

    Create NestedMap.sol

---
+++ NestedMap.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract NestedMapping 
+{
+    
+    mapping(address => mapping(uint => bool)) public nested;
+
+    function get(address _addr1, uint _i) public view returns (bool) 
+    {
+        return nested[_addr1][_i];
+    }
+
+    function set(
+        address _addr1,
+        uint _i,
+        bool _boo
+    ) public 
+    {
+        nested[_addr1][_i] = _boo;
+    }
+
+    function remove(address _addr1, uint _i) public 
+    {
+        delete nested[_addr1][_i];
+    }
+}
