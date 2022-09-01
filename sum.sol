17cc94057bf19c1b85d5601303a23f828a685d98
Gayathri Suresh <77912752+gayathrisuresh150501@users.noreply.github.com>
2022-09-01 22:18:31 +0530

    Create sum.sol

---
+++ sum.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+contract SumOfTwoNums
+{
+    constructor() public
+    {
+        
+    }
+    function sum() public view returns(int)
+    {
+        int a = 10;
+        int b = 5;
+        int result =  a + b;
+        return result;
+    }
+}
